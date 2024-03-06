537d55afdbc0c2b54590be71686d47ba9e9e36ae
Oğuzhan Baş <oguzhan.bas@bil.omu.edu.tr>
2024-03-06 13:58:07 +0300

    PDO ile CRUD işlemleri

--- bolum-17/index.php
+++ bolum-17/index.php
@@ -30,17 +30,75 @@
         //     print_r($urun);
         //     echo "<pre>";
         // }
+
+        // ----------prepared----------
         
-        $productId = 2;
-        $price = 45000;
-        $sql = "SELECT * FROM products WHERE price>? or id=?";
-        $statement = $pdo->prepare($sql);
-        $statement->execute([$price,$productId]);
-        $urunler = $statement->fetchAll();
+        // $productId = 2;
+        // $price = 45000;
+        // $sql = "SELECT * FROM products WHERE price>? or id=?";
+        // $statement = $pdo->prepare($sql);
+        // $statement->execute([$price,$productId]);
+        // $urunler = $statement->fetchAll();
         
-        foreach ($urunler as $urun) {
-            echo "<pre>";
-            print_r($urun);
-            echo "<pre>";
-        }
+        // foreach ($urunler as $urun) {
+        //     echo "<pre>";
+        //     print_r($urun);
+        //     echo "<pre>";
+        // }
+
+        // ----------insert data----------
+
+        // $title = "Discovery Mobil 4";
+        // $price = 17000;
+        // $description = "güzel mazili telefon";
+        
+        // $sql = "INSERT INTO products(title,price,description) VALUES(:title,:price,:description)";
+        // $stmt = $pdo->prepare($sql);
+        // $stmt ->execute(['title'=>$title,'price'=>$price,'description'=>$description]);
+        
+        // echo "kayıt eklendi...";
+        
+        // ----------multiple insert----------
+        
+        // $title = "Samsung Kalem";
+        // $price = 20000;
+        // $description = "güzel mazili telefon";
+        
+        // $sql = "INSERT INTO products(title,price,description) VALUES(:title,:price,:description)";
+        // $stmt = $pdo->prepare($sql);
+        
+        // $stmt ->bindParam(':title',$title);
+        // $stmt ->bindParam(':price',$price);
+        // $stmt ->bindParam(':description',$description);
+        
+        // $stmt->execute();
+        
+        // $title = "Samsung Kalem 4";
+        // $price = 20000;
+        // $description = "güzel mazili kedili telefon";
+        
+        // $stmt->execute();
+        
+        
+        // ----------kayıt güncelleme----------
+        
+        // $id=1;
+        // $title="updated";
+
+        // $sql = "UPDATE products SET title=:title WHERE id=:id";
+        // $stmt = $pdo->prepare($sql);
+        // $stmt->execute(['id'=>$id,'title'=>$title]);
+        
+        // echo "güncellendi: ".$stmt->rowCount();
+        
+        // ----------kayıt silme----------
+        
+        $id=1;
+
+        $sql = "DELETE FROM products WHERE id=:id";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(['id'=>$id]);
+        
+        echo "silindi: ".$stmt -> rowCount();
+
 ?>
